f7cc0908823b6928db8555387b25e5a11772da33
RaspberyPicardBox <raspberrypicardbox@gmail.com>
2021-02-22 13:26:24 +0000

    Began building walls

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from qibullet import SimulationManager
 from qibullet import PepperVirtual
+import numpy as np
 import pybullet
 import cv2
 import threading
@@ -19,19 +20,26 @@ if __name__ == "__main__":
     simulation_manager = SimulationManager()
     client_id = simulation_manager.launchSimulation(gui=True)
 
-    pybullet.loadURDF("objects\person.urdf", basePosition=[0, 3, 0], baseOrientation=[0, 0, 0, 1])
-
     pybullet.loadURDF("objects\wall.urdf", basePosition=[-1, -1.5, 0], baseOrientation=[1, 0, 0, 1], useFixedBase=1,
-                      physicsClientId=client_id)
+                      physicsClientId=client_id)  # Right Wall
+
+    pybullet.loadURDF("objects\wall.urdf", basePosition=[1, -1.5, 0], baseOrientation=[1, 0, 0, 1], useFixedBase=1,
+                      physicsClientId=client_id)  # Second Right Wall
 
     pybullet.loadURDF("objects\wall.urdf", basePosition=[-1, 2, 0], baseOrientation=[1, 0, 0, 1], useFixedBase=1,
-                      physicsClientId=client_id)
+                      physicsClientId=client_id)  # Left Wall
+
+    pybullet.loadURDF("objects\wall.urdf", basePosition=[1, 2, 0], baseOrientation=[1, 0, 0, 1], useFixedBase=1,
+                      physicsClientId=client_id)  # Second Left Wall
 
     pybullet.loadURDF("objects\wall.urdf", basePosition=[-1.5, -2, 0], baseOrientation=[1, 1, 1, 1], useFixedBase=1,
-                      physicsClientId=client_id)
+                      physicsClientId=client_id)  # Back Right
 
     pybullet.loadURDF("objects\wall.urdf", basePosition=[-1.5, 0, 0], baseOrientation=[1, 1, 1, 1], useFixedBase=1,
-                      physicsClientId=client_id)
+                      physicsClientId=client_id)  # Back Left
+
+    pybullet.loadURDF("objects\wall.urdf", basePosition=[2.5, -2, 0], baseOrientation=[1, 1, 1, 1], useFixedBase=1,
+                      physicsClientId=client_id)  # Internal Right
 
     pepper = simulation_manager.spawnPepper(
         client_id,
@@ -47,10 +55,10 @@ if __name__ == "__main__":
     try:
         while True:
             x = 1
-            pepper.moveTo(1.0, 0.0, 0.0, frame=PepperVirtual.FRAME_ROBOT, speed=100)
-            pepper.moveTo(0, 0, 3.14159, frame=PepperVirtual.FRAME_ROBOT)
-            pepper.moveTo(1.0, 0.0, 0.0, frame=PepperVirtual.FRAME_ROBOT, speed=100)
-            pepper.moveTo(0, 0, 3.14159, frame=PepperVirtual.FRAME_ROBOT)
+            pepper.moveTo(1.0, 0.0, 0.0, frame=PepperVirtual.FRAME_ROBOT)
+            pepper.moveTo(0, 0, np.radians(180), frame=PepperVirtual.FRAME_ROBOT)  # Turn is in radians
+            pepper.moveTo(1.0, 0.0, 0.0, frame=PepperVirtual.FRAME_ROBOT)
+            pepper.moveTo(0, 0, np.radians(180), frame=PepperVirtual.FRAME_ROBOT)
     except KeyboardInterrupt:
         pass
     finally:
